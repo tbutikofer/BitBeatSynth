c3553fcd1f742f42d7fb1346a9a8f8998b9b8164
Thomas Bütikofer <t.buetikofer.mail@gmail.com>
2025-06-06 23:19:24 +0200

    Undo Hex Grid Waveform

--- BitBeatSynth/ContentView.swift
+++ BitBeatSynth/ContentView.swift
@@ -66,13 +66,9 @@ struct ContentView: View {
                                 LiveCodingManualView()
                                     .edgesIgnoringSafeArea(.all)
                             } else {
-                                HexWaveformView(
-                                    samples: audio.waveformBuffer,
-                                    glowIntensity: 1.0,
-                                    cloudOpacity: 0.3,
-                                    updateSpeed: 0.05
-                                )
-                                .edgesIgnoringSafeArea(.all)
+                                WaveformView(samples: audio.waveformBuffer)
+                                    .opacity(0.25)
+                                    .edgesIgnoringSafeArea(.all)
                             }
 
                             TouchPad(
